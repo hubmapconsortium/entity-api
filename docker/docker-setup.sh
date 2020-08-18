bfa45d5c8160755dc5427734f3b3f089eb956951
yuanzhou <yuanzhou19@gmail.com>
2020-08-17 21:28:08 -0400

    Added VERSION for docker image build

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set the version environment variable for the docker build
+# Version number is from the VERSION file
+export ENTITY_API_VERSION=`cat VERSION`
+
+echo "ENTITY_API_VERSION: $ENTITY_API_VERSION"
+
 # Copy over the src folder
 cp -r ../src entity-api/
 
