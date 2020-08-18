8b290c67d3545eea366e162887540111e78d5569
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 10:28:33 -0400

    Remove newlines and spaces from VERSION file

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -2,7 +2,8 @@
 
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
-export ENTITY_API_VERSION=`cat ../VERSION`
+# Also remove newlines and leading/trailing slashes if present in that VERSION file
+export ENTITY_API_VERSION=$(tr -d "\n\r" < VERSION | xargs)
 
 echo "ENTITY_API_VERSION: $ENTITY_API_VERSION"
 
