bb9e1f041dc390b03c3898c081aebcc8d74d57af
yuanzhou <yuanzhou19@gmail.com>
2020-08-17 22:25:21 -0400

    Fixed wrong path

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -2,7 +2,7 @@
 
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
-export ENTITY_API_VERSION=`cat VERSION`
+export ENTITY_API_VERSION=`cat ../VERSION`
 
 echo "ENTITY_API_VERSION: $ENTITY_API_VERSION"
 
