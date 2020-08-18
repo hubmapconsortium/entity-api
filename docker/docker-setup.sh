ffbf7a792c589d7acd0ea64df4b6378496636734
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 10:39:52 -0400

    Fixed path error

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -3,7 +3,7 @@
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
-export ENTITY_API_VERSION=$(tr -d "\n\r" < VERSION | xargs)
+export ENTITY_API_VERSION=$(tr -d "\n\r" < ../VERSION | xargs)
 
 echo "ENTITY_API_VERSION: $ENTITY_API_VERSION"
 
