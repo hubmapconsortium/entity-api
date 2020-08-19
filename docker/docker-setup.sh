634c935e4a128507a8cce77395f3aee5a358ef10
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 23:10:46 -0400

    Added localhost build

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -4,7 +4,6 @@
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
 export ENTITY_API_VERSION=$(tr -d "\n\r" < ../VERSION | xargs)
-
 echo "ENTITY_API_VERSION: $ENTITY_API_VERSION"
 
 # Copy over the src folder
