c1d682033d3ead84b33eca2a8e63378f392e5284
yuanzhou <yuanzhou19@gmail.com>
2020-08-19 20:16:00 -0400

    Removed setup from script

--- docker/entity-api-docker.sh
+++ docker/entity-api-docker.sh
@@ -18,25 +18,10 @@ function export_version() {
 if [[ "$1" != "localhost" && "$1" != "dev" && "$1" != "test" && "$1" != "stage" && "$1" != "prod" ]]; then
     echo "Unknown build environment '$1', specify one of the following: localhost|dev|test|stage|prod"
 else
-    if [[ "$2" != "setup" && "$2" != "check" && "$2" != "config" && "$2" != "build" && "$2" != "start" && "$2" != "stop" && "$2" != "down" ]]; then
-        echo "Unknown command '$2', specify one of the following: setup|check|config|build|start|stop|down"
+    if [[ "$2" != "check" && "$2" != "config" && "$2" != "build" && "$2" != "start" && "$2" != "stop" && "$2" != "down" ]]; then
+        echo "Unknown command '$2', specify one of the following: check|config|build|start|stop|down"
     else
-        if [ "$2" = "setup" ]; then
-            # Copy over the source code to docker directory
-            cp -r ../src entity-api/
-        elif [ "$2" = "build" ]; then
-            export_version
-            docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
-        elif [ "$2" = "start" ]; then
-            export_version
-            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml up -d
-        elif [ "$2" = "stop" ]; then
-            export_version
-            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml stop
-        elif [ "$2" = "down" ]; then
-            export_version
-            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml down
-        elif [ "$2" = "check" ]; then
+        if [ "$2" = "check" ]; then
             # Bash array
             config_paths=(
                 '../src/instance/app.cfg'
@@ -58,7 +43,22 @@ else
             echo 'Checks complete, all good :)'
         elif [ "$2" = "config" ]; then
             export_version
-    		docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml config
+            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml config
+        elif [ "$2" = "build" ]; then
+            # Copy over the source code to docker directory
+            cp -r ../src entity-api/
+
+            export_version
+            docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
+        elif [ "$2" = "start" ]; then
+            export_version
+            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml up -d
+        elif [ "$2" = "stop" ]; then
+            export_version
+            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml stop
+        elif [ "$2" = "down" ]; then
+            export_version
+            docker-compose -p entity-api -f docker-compose.yml -f docker-compose.$1.yml down
         fi
     fi
 fi
