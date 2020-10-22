bd1d7ad6240a9d4f894cb6f69f90e93fd9dfee71
yuanzhou <yuanzhou19@gmail.com>
2020-10-22 09:37:17 -0400

    Separate module for trigger methods

---
+++ src/trigger_methods.py
@@ -0,0 +1,158 @@
+import logging
+
+####################################################################################################
+## Schema trigger methods based on the yaml file - DO NOT RENAME
+####################################################################################################
+
+"""
+Trigger event method of generating current timestamp
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    A timestamp string
+"""
+def get_current_timestamp(data_dict):
+    current_time = datetime.datetime.now() 
+    return current_time.timestamp() 
+
+"""
+Trigger event method of generating current timestamp
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The string of normalized entity type
+"""
+def get_entity_type(data_dict):
+    return data_dict['normalized_entity_type']
+
+"""
+Trigger event method of getting user sub
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The 'sub' string
+"""
+def get_user_sub(data_dict):
+    return data_dict['sub']
+
+"""
+Trigger event method of getting user email
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The 'email' string
+"""
+def get_user_email(data_dict):
+    return data_dict['email']
+
+"""
+Trigger event method of getting user name
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The 'name' string
+"""
+def get_user_name(data_dict):
+    return data_dict['name']
+
+"""
+Trigger event method of getting uuid
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The uuid string
+"""
+def create_uuid(data_dict):
+    return data_dict['hmuuid']
+
+"""
+Trigger event method of getting uuid
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The doi_suffix_id string
+"""
+def create_doi_suffix_id(data_dict):
+    return data_dict['doi_suffix_id']
+
+"""
+Trigger event method of getting uuid
+
+Parameters
+----------
+data_dict : dict
+    A merged dictionary that contains all possible input data to be used
+    It's fine if a trigger method doesn't use any input data
+
+Returns
+-------
+string
+    The hubmap_id string
+"""
+def create_hubmap_id(data_dict):
+    return data_dict['hubmap_id']
+
+# To-DO
+def get_dataset_uuids(data_dict):
+    return "dummy"
+
+# To-DO
+def fill_creators(data_dict):
+    return "dummy"
+
+# To-DO
+def fill_contacts(data_dict):
+    return "dummy"
+
+# To-DO
+def get_data_access_level(data_dict):
+    return "public"
