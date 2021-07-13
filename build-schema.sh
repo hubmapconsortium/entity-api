3dda5a5fbb954a6715a26b8944ecc58f831d4aaf
DerekFurstPitt <drf57@pitt.edu>
2021-07-13 12:06:17 -0400

    Fixed error checking such that most of it occurs in the shell script build-schema.sh rather than the python script general_schema_template_transformer.py. This way, we can avoid running the script at all if errors are found. If errors still are encountered at input and output, exception handling exists in the python script. I removed a ton of previously commented out lines to get this ready for final review, and changed variable names to be less redundant and more descriptive. I had an integer count loops through a recursive variable and that has now been replaced. Lastly, I removed a potential source of a future bug. Since adding a third return type for my recursive function, I had forgotten to change an earlier comparator that was either "equal" to a value or "not equal". Now that there's a third possible output, there was a case where something incorrect could be appended to a dictionary. This has been corrected.

--- build-schema.sh
+++ build-schema.sh
@@ -2,6 +2,18 @@
 
 FILE="$1"
 SCRIPTDIRECTORY=$(dirname ${BASH_SOURCE[0]})
-${SCRIPTDIRECTORY}/general_schema_template_transformer.py $FILE > ${SCRIPTDIRECTORY}/my-spec.yaml
-#cat ${SCRIPTDIRECTORY}/new-spec-api.yaml > ${SCRIPTDIRECTORY}/my-spec.yaml
-#rm ${SCRIPTDIRECTORY}/new-spec-api.yaml
+if (( $# == 1)); then
+  if test -f "$FILE"; then
+    ${SCRIPTDIRECTORY}/general_schema_template_transformer.py $FILE > ${SCRIPTDIRECTORY}/my-spec.yaml
+    var=$( cat ${SCRIPTDIRECTORY}/my-spec.yaml)
+  else
+    echo "$FILE does not exist"
+  fi
+else
+  echo "$# arguments were passed. Please pass exactly 1 argument that is the name of an existing file"
+fi
+if test -f "./my-spec.yaml"; then
+  if test -z "$var"; then
+    rm $SCRIPTDIRECTORY/my-spec.yaml
+  fi
+fi
--- general_schema_template_transformer.py
+++ general_schema_template_transformer.py
@@ -4,7 +4,7 @@ import yaml
 import sys
 
 #This program takes in as an argument a reference to a yaml file. It will then scan through the file looking for
-# #particular tags, and then upon finding those tags, replacing them with text from other yaml files a given address
+# #particular tags, and then upon finding those tags, replacing them with text from other yaml files of a given address
 
 #This function accepts a file path to a yaml file and returns the contents of the yaml file as a dictionary
 def input_from_yaml(inputfile):
@@ -12,109 +12,75 @@ def input_from_yaml(inputfile):
         with open(inputfile) as file:
             yaml_template = yaml.load(file, Loader=yaml.FullLoader)
             return yaml_template
-    except FileNotFoundError as e:
-        print(e)
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.exit()
 #This function accepts a url for a yaml file, and returns the contents of the yaml file in a python dictionary
 def get_yaml_from_url(yaml_url):
-    with urllib.request.urlopen(yaml_url) as urlfile:
-        yaml_resource_file = yaml.load(urlfile, Loader=yaml.FullLoader)
-        return yaml_resource_file
-#This function accepts a python dictionary and outputs a yaml file to a given file path with the content of that library inside.
+    try:
+        with urllib.request.urlopen(yaml_url) as urlfile:
+            yaml_resource_file = yaml.load(urlfile, Loader=yaml.FullLoader)
+            return yaml_resource_file
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.exit()
+#This function accepts a python dictionary and outputs a yaml file to stdout.
 def output_to_yaml():
-    yaml.safe_dump(outputyaml, sys.stdout, allow_unicode=True, default_flow_style=False, sort_keys=False)
-    #with open('new-spec-api.yaml', 'w') as outfile:
-    #   yaml.dump(outputyaml, outfile, sort_keys=False)
+    try:
+        yaml.safe_dump(outputyaml, sys.stdout, allow_unicode=True, default_flow_style=False, sort_keys=False)
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.exit()
 #This function takes in a strongly nested dictionary, then recursively traverses it looking for certain tags. It then replaces the tags with text from other files and then returns a new file.
 def create_new_yaml(nested_dict):
     emptydict = {}
-    for mykey, myvalue in nested_dict.items():
-        mykeystring = str(mykey)
-        #storagedict.clear()  trying to make sure this clears out in case it needs to run through again and needs to be empty
-        if mykeystring.startswith('X-replace')==False:
-            if type(myvalue) != dict:
-                emptydict[mykey] = myvalue
-            if type(myvalue) == dict:
-                recursivevalue = create_new_yaml(myvalue)
-                if recursivevalue != 2:
-                    emptydict[mykey] = recursivevalue
-                if recursivevalue ==2:
-                    emptydict[mykey]=storagedict
-                if recursivevalue ==3:
+    for key, value in nested_dict.items():
+        keystring = str(key)
+        if keystring.startswith('X-replace')==False:
+            if type(value) != dict:
+                emptydict[key] = value
+            if type(value) == dict:
+                recursivevalue = create_new_yaml(value)
+                if recursivevalue != "schema replaced" and recursivevalue !="enum replaced":
+                    emptydict[key] = recursivevalue
+                if recursivevalue == "schema replaced":
+                    emptydict[key]=storagedict
+                if recursivevalue =="enum replaced":
                     emptydict['enum'] = secondstorage.get('enum')
-        if mykeystring.startswith('X-replace'):
-            if mykeystring == 'X-replace-enum-list':
-                yaml_url=str(myvalue.get('enum-file-ref'))
+        if keystring.startswith('X-replace'):
+            if keystring == 'X-replace-enum-list':
+                yaml_url=str(value.get('enum-file-ref'))
                 if yaml_url.startswith('http') == False:
                     yaml_obj = input_from_yaml(yaml_url)
                 if yaml_url.startswith('http'):
                     yaml_obj = get_yaml_from_url(yaml_url)
                 replaced_section = []
-                for key, value in yaml_obj.items():
-                    replaced_section.append(key)
-                # emptydict['enum'] = replaced_section
+                for enumkey, enumvalue in yaml_obj.items():
+                    replaced_section.append(enumkey)
                 secondstorage['enum'] = replaced_section
-                return 3
-            if mykeystring == 'X-replace-schema':
-                yaml_url_list=(myvalue.get('schema-file-ref'))
-                for thatkey, thatvalue in emptydict.items():
-                    storagedict[thatkey] = thatvalue
-                for thisitem in yaml_url_list:
-                    yaml_url = str(thisitem)
+                return "enum replaced"
+            if keystring == 'X-replace-schema':
+                yaml_url_list=(value.get('schema-file-ref'))
+                for schemakey, schemavalue in emptydict.items():
+                    storagedict[schemakey] = schemavalue
+                for thisurl in yaml_url_list:
+                    yaml_url = str(thisurl)
                     if yaml_url.startswith('http') == False:
                         yaml_obj = input_from_yaml(yaml_url)
                     if yaml_url.startswith('http'):
                         yaml_obj = get_yaml_from_url(yaml_url)
-                    #for tempkey, tempvalue in yaml_obj.items():
-                    #    storagedict[tempkey] = tempvalue
                     internalcall = create_new_yaml(yaml_obj)
                     for tempkey, tempvalue in internalcall.items():
                         storagedict[tempkey] = tempvalue
-                return 2
-
-
+                return "schema replaced"
     return emptydict
-
-#if len(sys.argv)>1: #Makes sure that there is at least one argument being passed to this program. If there is, the argument is assigned to a variable. input_from_yaml is then called with this variable as a parameter.
-#   input_file = str(sys.argv[1])
-try:
-    input_file = str(sys.argv[1])
-    yaml_template = input_from_yaml(input_file)
-except Exception as e:
-    print(e)
-
-
+if len(sys.argv)>1: #Makes sure that there is at least one argument being passed to this program. If there is, the argument is assigned to a variable. input_from_yaml is then called with this variable as a parameter.
+   input_file = str(sys.argv[1])
+else:
+    print("Must pass in exactly 1 argument: the name of an existing file")
+yaml_template = input_from_yaml(input_file)
 
 storagedict = {} #Instantiates a global dictionary. This provides temporary storage of dictionary elements to be used with create_new_yaml function
 secondstorage = {} #Instantiates a second global dictionary. This is only used for collecting enum lists to use at earlier loops through the recursive function
 outputyaml = create_new_yaml(yaml_template)
 output_to_yaml()
-
-
-
-#def demo_recurive_algorithm()
-    #for item in nested_lookup.nested_lookup('X-replace-schema', yaml_template):
-    #    yaml_url=str(item.get('schema-file-ref'))
-    #    if yaml_url.startswith('http'):
-    #        yaml_resource_file = get_yaml_from_url(yaml_url)
-    #    if yaml_url.startswith('http') == False:
-    #        yaml_resource_file = input_from_yaml(yaml_url)
-
-        #for item in nested_lookup.nested_lookup('schemas', yaml_resource_file):
-        #for key, value in yaml_resource_file.items():
-        #    #if str(key).startswith('X-replace') or str(value).startswith('X-replace') ==False:
-        #    print(key)
-        #    print(value)
-        #        #emptydict.append(key:value)
-
-
-    #yaml_resource_file = get_yaml_from_url()
-    #nested_lookup.nested_lookup('Donor',yaml_resource_file)
-
-    #for item in nested_lookup.nested_lookup('X-replace-enum-list',yaml_template): #for every item in the dictionary yaml_template, it searches for a specfic phrase
-    #    yaml_url=item.get('enum-file-ref')
-    #    yaml_resource_file = get_yaml_from_url()
-    #    replaced_section = [] #instantiates an empty list
-    #    for key, value in yaml_resource_file.items():#fills the list with the keys from yaml_resource_file
-    #        replaced_section.append(key) #for each key in the dictionary, add it to the list
-    #    recursive_nested_dictionary_iterator(yaml_template, yaml_url) #calls the recursive program
-
