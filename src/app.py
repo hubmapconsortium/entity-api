1a413601416ff640916781ce5e1880e946179ab4
Bill Shirey <shirey@pitt.edu>
2021-12-10 10:56:51 -0500

    create second /entities/ route for revisions method

--- src/app.py
+++ src/app.py
@@ -2075,6 +2075,7 @@ Returns
 list
     The list of revision datasets
 """
+@app.route('/entities/<id>/revisions', methods=['GET'])
 @app.route('/datasets/<id>/revisions', methods=['GET'])
 def get_revisions_list(id):
     # By default, do not return dataset. Only return dataset if return_dataset is true
@@ -2097,7 +2098,7 @@ def get_revisions_list(id):
 
     # Only for Dataset
     if normalized_entity_type != 'Dataset':
-        bad_request_error("The entity of given id is not a Dataset")
+        bad_request_error("The entity is not a Dataset. Found entity type:" + normalized_entity_type)
 
     # Only published/public datasets don't require token
     if entity_dict['status'].lower() != DATASET_STATUS_PUBLISHED:
