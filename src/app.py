7e419f5c89fb89f7f70bd57a709f5763d266ea93
yuanzhou <yuanzhou19@gmail.com>
2022-03-18 11:24:02 -0400

    Python app logs to file

--- src/app.py
+++ src/app.py
@@ -30,11 +30,17 @@ from hubmap_commons.hm_auth import AuthHelper
 from hubmap_commons.exceptions import HTTPException
 
 
-# Set logging fromat and level (default is warning)
-# All the API logging is forwarded to the uWSGI server and gets written into the log file `uwsgo-entity-api.log`
+# All the API logging is gets written into the log file 
+# The uWSGI logging for each deployment disables the request logging 
+# but still captures the 4xx and 5xx errors to the file `log/uwsgi-entity-api.log`
 # Log rotation is handled via logrotate on the host system with a configuration file
 # Do NOT handle log file and rotation via the Python logging to avoid issues with multi-worker processes
-logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename='../log/entity-api-' + time.strftime("%m-%d-%Y-%H-%M-%S") + '.log',
+                    filemode='a',
+                    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 # Specify the absolute path of the instance folder and use the config file relative to the instance path
