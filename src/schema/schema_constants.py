2a508b50c6de2bb7303d956eca9a96f18103e31f
kburke <209327+kburke@users.noreply.github.com>
2026-01-09 16:14:51 -0500

    Grammar correction

--- src/schema/schema_constants.py
+++ src/schema/schema_constants.py
@@ -65,7 +65,7 @@ class Neo4jRelationshipEnum(Enum):
     USES_DATA = 'USES_DATA'
 
 # Define an enumeration of re-index priority level types.
-# N.B. This is the same values maintained in ingest-api app.py _get_reindex_priority(), which
+# N.B. These are the same values maintained in ingest-api app.py _get_reindex_priority(), which
 #      must be the same levels defined for the enqueue() method at
 #      https://github.com/x-atlas-consortia/jobq/blob/main/src/atlas_consortia_jobq/queue.py
 class ReindexPriorityLevelEnum(Enum):
