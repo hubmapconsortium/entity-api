43a614d714f77770bbfc95320f588f5c0fd0dfe7
DerekFurstPitt <drf57@pitt.edu>
2026-03-20 16:56:17 -0400

    added test directory locust_test with some test case files. Updated uwsgi and nginx configs to help with 502 errors

---
+++ test/locust_test/get_entity_test.py
@@ -0,0 +1,21 @@
+from locust import HttpUser, task, constant
+import json
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+with open("ids.json") as f:
+    valid_ids = json.load(f)
+
+class GetEntityUser(HttpUser):
+    wait_time = constant(0)
+    token = os.getenv("TOKEN")
+
+    @task
+    def hit_authenticated_endpoint(self):
+        entity_id = random.choice(valid_ids)
+        headers = {"Authorization": f"Bearer {self.token}"}
+        self.client.get(f"/entities/{entity_id}", headers=headers)
+
+    
